c7a4ba654af6d1a7c630558e8454291872fd7a02
unknown <perceptionarmy@icloud.com>
2023-03-20 03:05:20 +0700

    image to text

--- 2.image_to_text/main.py
+++ 2.image_to_text/main.py
@@ -4,7 +4,14 @@ from PIL import Image
 img = Image.open("img.png")
 pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
 
-custom_config = r'--oem 3 --psm 13'
+file_name = img.filename
+file_name = file_name.split(".")[0]
+
+
+custom_config = r'--oem 3 --psm 6'
 
 text = pytesseract.image_to_string(img, config=custom_config)
 print(text)
+
+with open(f'{file_name}.txt', 'w') as text_file:
+    text_file.write(text)
