ab7831e0e0a1030645024f8a49c23c17e91a99a9
unknown <perceptionarmy@icloud.com>
2023-03-20 02:19:13 +0700

    enc/dec

---
+++ 1.crypt/decryption.py
@@ -0,0 +1,29 @@
+import pyAesCrypt
+import os
+
+def decryption(file, password):
+
+    buffer_size = 512 * 1024
+
+    pyAesCrypt.decryptFile(str(file), str(os.path.splitext(file)[0]), password, buffer_size)
+
+    print("[файл ' " + str(os.path.splitext(file)[0]) +  "' дешифрован]")
+
+    os.remove(file)
+
+
+def walking_by_dirs(dir, password):
+    for name in os.listdir(dir):
+        path = os.path.join(dir, name)
+
+        if os.path.isfile(path):
+            try:
+                decryption(path, password)
+            except Exception as ex:
+                print(ex)
+        else:
+            walking_by_dirs(path, password)
+
+
+password = input("введите пароль для дешифровки")
+walking_by_dirs("C:/py/forlder", password)
