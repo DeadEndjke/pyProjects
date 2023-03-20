aa7b2c016f17a193e95bda317ade121039ef16f2
unknown <perceptionarmy@icloud.com>
2023-03-20 11:52:26 +0700

    steganography

---
+++ 3.steganography/main.py
@@ -0,0 +1,37 @@
+
+#---------------------------------------------первый вариант
+
+
+# from stegano import lsb
+
+# secret = lsb.hide("img.png", "13Horotu")
+# secret.save("img_secret.png") 
+
+
+# result = lsb.reveal("img_secret.png")
+# print(result)
+
+#---------------------------------------------второй вариант
+
+# from stegano import exifHeader
+
+# secret = exifHeader.hide("img.png", "img_secret2.png", "73ibunot")
+
+# result = exifHeader.reveal("img_secret2.png").decode()
+
+# print(result)
+
+#---------------------------------------------вариант с ключом
+
+from steganocryptopy.steganography import Steganography
+
+
+Steganography.generate_key("")
+
+secret = Steganography.encrypt("key.key", "img.png", "secret_message.txt")
+secret.save("img_secret3.png")
+
+
+result = Steganography.decrypt("key.key", "img_secret3.png")
+
+print(result)
